d02953aa85ca8873645787b89833db145f21bf83
Sagrado, Princess Genevieve <xhanhime@gmai.com>
2023-08-08 16:43:34 +0800

    Fixed Components and Assets

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,21 +4,22 @@ import GradientBackground from '../components/GradientBackground';
 import ConstButton from '../components/ConstButton';
 import LogOutButton from '../components/LogOutButton';
 import ImagePicker from 'react-native-image-crop-picker';
-import axios from 'axios';
 import Clipboard from '@react-native-clipboard/clipboard';
 import TextRecognition from 'react-native-text-recognition';
 
+
+const API_KEY = 'a78e7328427e5da24f4e244acf557715'; // Replace with your Mindee API key
+
 const DEFAULT_HEIGHT = 1200;
-const DEFAULT_WITH = 600;
+const DEFAULT_WIDTH = 600;
 const defaultPickerOptions = {
   cropping: true,
   height: DEFAULT_HEIGHT,
-  width: DEFAULT_WITH,
+  width: DEFAULT_WIDTH,
 };
 
 export default function HomePage({ onPress }) {
   const [isLoading, setIsLoading] = useState(false);
-  const [imgSrc, setImgSrc] = useState(null);
   const [ocrDetails, setOcrDetails] = useState('');
 
   const copyToClipboard = () => {
@@ -27,132 +28,54 @@ export default function HomePage({ onPress }) {
 
   const recognizeTextFromImage = async (path) => {
     setIsLoading(true);
-
+  
     try {
       const recognizedText = await TextRecognition.recognize(path);
-
-      const values = {
-        name: extractValue(/Last Name, First Name, Middle Name\n(.*?)(?:\n|$)/s, recognizedText),
-        nationality: extractValue(/Nationality\n(.*?)(?:\n|$)/s, recognizedText),
-        sex: extractValue(/Sex\n(.*?)(?:\n|$)/s, recognizedText),
-        dob: extractValue(/Date of Birth\n(.*?)(?:\n|$)/s, recognizedText),
-        weight: extractValue(/Weight \(kg\)\n(.*?)(?:\n|$)/s, recognizedText),
-        height: extractValue(/Height\(m\)\n(.*?)(?:\n|$)/s, recognizedText),
-        address: extractValue(/Address\n(.*?)(?:\n|$)/s, recognizedText),
-        licenseNo: extractValue(/License No\.\n(.*?)(?:\n|$)/s, recognizedText),
-        expirationDate: extractValue(/Expiration Date\n(.*?)(?:\n|$)/s, recognizedText),
-        agencyCode: extractValue(/Agency Code\n(.*?)(?:\n|$)/s, recognizedText),
-        bloodType: extractValue(/Blood Type\n(.*?)(?:\n|$)/s, recognizedText),
-        eyeColor: extractValue(/Eyes Color\n(.*?)(?:\n|$)/s, recognizedText),
-        dlCodes: extractValue(/DL Codes\n(.*?)(?:\n|$)/s, recognizedText),
-        condition: extractValue(/Condition\n(.*?)(?:\n|$)/s, recognizedText),
+      console.log('Recognized Text:', recognizedText); // Log the OCR text to the console
+  
+      // Define field labels and their variations (heuristics)
+      const fieldLabels = {
+        lastname: [/Apelyido\/Last Name:?\s+(.*)/i, /Apelyido\/Lastname:?\s+(.*)/i],
+        givenNames: [/Mga Pangalan\/Given Names:?\s+(.*)/i, /Mga Pangalan\/Given\s*Names:?\s+(.*)/i],
+        middleName: [/Gitnang Apelyido\/Middle Name:?\s+(.*)/i, /Gitnang Apelyido\/Middle\s*Name:?\s+(.*)/i],
+        dob: [/Petsa ng Kapanganakan\/Date of Birth:?\s+(.*)/i, /Petsa\s*ng\s*Kapanganakan\/Date\s*of\s*Birth:?\s+(.*)/i],
       };
-
-      // Validate and format the values
-      const formattedValues = {
-        name: values.name.length > 5 ? values.name : 'Not found',
-        nationality: values.nationality === 'PHL' ? values.nationality : 'Not found',
-        sex: ['F', 'M'].includes(values.sex) ? values.sex : 'Not found',
-        dob: /^\d{4}\/\d{2}\/\d{2}$/.test(values.dob) ? values.dob : 'Not found',
-        weight: /^\d+(\.\d+)?$/.test(values.weight) ? values.weight : 'Not found',
-        height: /^\d+(\.\d+)?$/.test(values.height) ? values.height : 'Not found',
-        address: values.address || 'Not found',
-        licenseNo: formatLicenseNo(values.licenseNo),
-        expirationDate: /^\d{4}\/\d{2}\/\d{2}$/.test(values.expirationDate) ? values.expirationDate : 'Not found',
-        agencyCode: formatAgencyCode(values.agencyCode),
-        bloodType: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'].includes(values.bloodType)
-          ? values.bloodType
-          : 'Not found',
-        eyeColor: ['Brown', 'Blue', 'Green', 'Hazel', 'Gray', 'Amber'].includes(values.eyeColor)
-          ? values.eyeColor
-          : 'Not found',
-        dlCodes: validateDLCodes(values.dlCodes) ? values.dlCodes : 'Not found',
-        condition: validateCondition(values.condition) ? values.condition : 'Not found',
-      };
-
-      // Concatenate the formatted values and set in the state
-      setOcrDetails(
-        `Last Name, First Name, Middle Name: ${formattedValues.name}\n` +
-        `Nationality: ${formattedValues.nationality}\n` +
-        `Sex: ${formattedValues.sex}\n` +
-        `Date of Birth: ${formattedValues.dob}\n` +
-        `Weight (kg): ${formattedValues.weight}\n` +
-        `Height (m): ${formattedValues.height}\n` +
-        `Address: ${formattedValues.address}\n` +
-        `License No.: ${formattedValues.licenseNo}\n` +
-        `Expiration Date: ${formattedValues.expirationDate}\n` +
-        `Agency Code: ${formattedValues.agencyCode}\n` +
-        `Blood Type: ${formattedValues.bloodType}\n` +
-        `Eyes Color: ${formattedValues.eyeColor}\n` +
-        `DL Codes: ${formattedValues.dlCodes}\n` +
-        `Condition: ${formattedValues.condition}`
-      );
+  
+      // Extract field values using heuristics
+      const extractedFields = Object.keys(fieldLabels).reduce((acc, field) => {
+        const regexes = fieldLabels[field];
+        for (const regex of regexes) {
+          const match = recognizedText.match(regex);
+          if (match) {
+            acc[field] = match[1].replace(/\s+/g, ' ').trim();
+            break;
+          }
+        }
+        return acc;
+      }, {});
+  
+      // Build the OCR details string
+      const ocrDetails = `Apelyido/Lastname: ${extractedFields.lastname || 'Not found'}\n` +
+        `Mga Pangalan/Given Names: ${extractedFields.givenNames || 'Not found'}\n` +
+        `Gitnang Apelyido/Middle Names: ${extractedFields.middleName || 'Not found'}\n` +
+        `Petsa ng Kapanganakan/Date of Birth: ${extractedFields.dob || 'Not found'}\n`;
+  
+      setOcrDetails(ocrDetails);
     } catch (err) {
       console.error(err);
       setOcrDetails('');
     }
-
+  
     setIsLoading(false);
   };
+  
+  
+  
+  
 
   const extractValue = (regex, text) => {
     const match = regex.exec(text);
-    return match && match[1] ? match[1].replace(/\n/g, '').trim() : '';
-  };
-
-  const formatLicenseNo = (licenseNo) => {
-    // Assuming the format is Region Code:Year Code:Sequence Number
-    const [regionCode, yearCode, sequenceNumber] = licenseNo.split('-');
-    return `${regionCode}${yearCode}-${sequenceNumber}`;
-  };
-
-  const formatAgencyCode = (agencyCode) => {
-    // Assuming the agency code format is a single letter followed by two digits
-    return /^[A-Z]\d{2}$/.test(agencyCode) ? agencyCode : 'Not found';
-  };
-
-  const validateDLCodes = (dlCodes) => {
-    // Assuming DL Codes are comma-separated and contain only valid values
-    const validDLCodes = ['A', 'A1', 'B', 'B1', 'B2', 'C', 'D', 'BE', 'CE'];
-    const codes = dlCodes.split(',').map((code) => code.trim());
-    return codes.every((code) => validDLCodes.includes(code));
-  };
-
-  const validateCondition = (condition) => {
-    // Assuming valid conditions are listed
-    const validConditions = [
-      'Restriction 1*',
-      'Restriction 2*',
-      'Restriction 3',
-      'Restriction 4*',
-      'Restriction 5',
-      'Restriction 6',
-      'Restriction 7',
-      'Restriction 8',
-      'A',
-      'A2',
-      'B',
-      'B1',
-      'B2',
-      'C',
-      'D',
-      'BE',
-      'none',
-      'None',
-    ];
-    return validConditions.includes(condition);
-  };
-
-  const recognizeFromPicker = async (options = defaultPickerOptions) => {
-    try {
-      const image = await ImagePicker.openPicker(options);
-      setImgSrc({ uri: image.path });
-      await recognizeTextFromImage(image.path);
-    } catch (err) {
-      if (err.message !== 'User cancelled image selection') {
-        console.error(err);
-      }
-    }
+    return match && match[1] ? match[1].trim() : '';
   };
 
   const recognizeFromCamera = async (options = defaultPickerOptions) => {
@@ -164,7 +87,6 @@ export default function HomePage({ onPress }) {
       };
 
       const image = await ImagePicker.openCamera(cameraOptions);
-      setImgSrc({ uri: image.path });
       await recognizeTextFromImage(image.path);
     } catch (err) {
       if (err.message !== 'User cancelled image selection') {
@@ -172,6 +94,7 @@ export default function HomePage({ onPress }) {
       }
     }
   };
+  
 
   return (
     <View style={styles.container}>
@@ -188,13 +111,16 @@ export default function HomePage({ onPress }) {
       {/* Display OCR Details */}
       {ocrDetails !== '' && (
         <View style={styles.ocrDetailsContainer}>
-          <Text style={styles.ocrDetailsTitle}>OCR Details:</Text>
-          <Text style={styles.ocrDetailsText}>{ocrDetails}</Text>
-          <Button onPress={() => setOcrDetails('')} title="BACK"></Button>
-          <TouchableOpacity style={styles.copyButton} onPress={copyToClipboard}>
-            <Text style={styles.copyButtonText}>Copy to Clipboard</Text>
-          </TouchableOpacity>
-        </View>
+
+        <Text style={styles.ocrDetailsTitle}>OCR Details:</Text>
+
+        <Text style={styles.ocrDetailsText}>{ocrDetails}</Text>
+
+        <Button onPress={() => setOcrDetails('')} title="BACK"></Button>
+        <TouchableOpacity style={styles.copyButton} onPress={copyToClipboard}>
+          <Text style={styles.copyButtonText}>Copy to Clipboard</Text>
+        </TouchableOpacity>
+      </View>
       )}
     </View>
   );
@@ -227,6 +153,8 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   ocrDetailsText: {
-    fontSize: 16,
+    fontSize: 10,
+    backgroundColor:"white",
+    color:"black"
   },
 });
